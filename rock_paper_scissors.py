8c2a6d8d346275bdca2ffb26ae4d9d6b1e2857b4
alfianadityads <alfianadityadwi@gmail.com>
2023-12-14 03:02:24 +0700

    make player indicator and buttons

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -28,13 +28,23 @@ comp_label = Label(root,image=scissors_comp_img, bg="#918f8e")
 user_label.grid(row=1,column=0)
 comp_label.grid(row=1,column=4)
 
-# Scores
+# Player Indicators
+userIndicator = Label(root, font=50, text="USER", bg="#918f8e", fg="white")
+compIndicator = Label(root, font=50, text="COMPUTER", bg="#918f8e", fg="white")
 
+userIndicator.grid(row=0, column=1)
+compIndicator.grid(row=0, column=3)
+
+# Scores
 userScores = Label(root, text=0, font=100, bg="#918f8e", fg="white")
 compScores = Label(root, text=0, font=100, bg="#918f8e", fg="white")
 
 userScores.grid(row=1, column=1)
 compScores.grid(row=1, column=3)
 
+# Buttons
+rock = Button(root, width=20, height=3, text="ROCK", bg="#daf2f2", fg="black").grid(row=2, column=1)
+paper = Button(root, width=20, height=3, text="PAPER", bg="#f7e702", fg="black").grid(row=2, column=2)
+scissor = Button(root, width=20, height=3, text="SCISSOR", bg="#ff8f8f", fg="black").grid(row=2, column=3)
 
 root.mainloop()
